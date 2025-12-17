e60bae6b8bd7a52a98f2544ef735262c0aa41b80
Alamin Refat <alaminrefat2017@gmail.com>
2025-12-17 09:01:27 +0600

    distinct and order by problem solve

--- sql/basic/2605-Executive Representatives.sql
+++ sql/basic/2605-Executive Representatives.sql
@@ -1,4 +1,4 @@
-# 2605 - Executive Representatives
+# 2605-Executive Representatives
 /*The financial sector needs a report on the providers of the products we sell. The reports include all categories, but for some reason, providers of products whose category is the executive, are not in the report.
 Your job is to return the names of the products and providers whose category ID is 6.*/
 
@@ -9,3 +9,4 @@ FROM products p
 INNER JOIN providers pr ON p.id_providers = pr.id
 INNER JOIN categories c ON p.id_categories = c.id
 WHERE c.id = 6;
+
---
+++ sql/basic/2607-Providers' City in Alphabetical Order.sql
@@ -0,0 +1,8 @@
+# 2607-Providers City in Alphabetical Order
+
+/*Every month the company asks for a report from the cities that providers are registered. So, do a query that returns all the cities of the providers, but in alphabetical order.
+OBS: You must not show repeated cities.*/
+
+SELECT DISTINCT city 
+FROM providers
+ORDER BY city; 
