4b61f4459fec7742d1fbf247932bc6e2db674f7e
Alamin Refat <alaminrefat2017@gmail.com>
2025-12-21 19:17:02 +0600

    hard date related problem

---
+++ sql/basic/2614-September Rentals.sql
@@ -0,0 +1,18 @@
+# 2614-September Rentals
+
+/*The video store is making its semi-annual report and needs your help. All you have to do is select the name of the clients and the date of rental, from the rentals made in September 2016.*/
+
+SELECT 
+    c.name,
+    r.rentals_date
+FROM 
+    customers c
+INNER JOIN 
+    rentals r ON c.id = r.id_customers
+WHERE 
+    EXTRACT(YEAR FROM r.rentals_date) = 2016
+    AND EXTRACT(MONTH FROM r.rentals_date) = 9
+ORDER BY 
+    r.rentals_date;
+
+    
