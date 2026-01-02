f739504afde06e274a8869a3fa4b4da79064fdd3
Alamin Refat <alaminrefat2017@gmail.com>
2026-01-02 17:42:25 +0600

    find id virus

---
+++ sql/basic/2746-Viruses.sql
@@ -0,0 +1,10 @@
+# 2746-Viruses
+
+/*Viruses are evolving, but new research has proven that by switching some proteins the vaccine becomes unbeatable. The protein H1(Hemagglutinin) when replaced by the X protein (Xenomorphina) has interesting effects against almost every viral disease. Some conspiracists say that after the vaccine’s discovery some strange 3 meters tall creatures were found in the surroundings of the laboratories, but this is clearly a lie.
+Therefore, you must replace every string “H1”( Hemagglutinin ) by 'X' ( Xenomorphina ).*/
+
+SELECT 
+    REPLACE(name, 'H1', 'X') AS name
+FROM virus
+ORDER BY id;
+
