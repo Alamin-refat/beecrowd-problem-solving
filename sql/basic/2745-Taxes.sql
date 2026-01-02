ef5634c17f8acc3289a7ce7521492fa9d71a9735
Alamin Refat <alaminrefat2017@gmail.com>
2026-01-02 17:38:01 +0600

    tax find

---
+++ sql/basic/2745-Taxes.sql
@@ -0,0 +1,11 @@
+# 2745-Taxes
+
+/*You are going to the International Personal Tax meeting and your proposal is: every individual with income higher than 3000 must pay a tax to the government, which is 10% of his/her income.
+Show the name and the tax value of each person who earns more than 3000, with two decimal places of precision.*/
+
+SELECT 
+    name,
+    ROUND(salary * 0.10, 2) AS tax
+FROM people
+WHERE salary > 3000
+ORDER BY id;
