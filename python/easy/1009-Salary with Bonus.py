93f8116369b22ef84d1765cadae3cd2a648d8e79
Alamin Refat <alaminrefat2017@gmail.com>
2026-05-22 21:12:19 +0600

    problem solved

--- python/easy/1009-Salary with Bonus.py
+++ python/easy/1009-Salary with Bonus.py
@@ -13,6 +13,5 @@ Print the seller's total salary, according to the given example.'''
 name = input()
 salary = float(input())
 sales = float(input())
-
 total = salary + (sales * 0.15)
 print(f"TOTAL = R$ {total:.2f}")
